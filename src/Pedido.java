4a44f060e3118c1d332f9e1996103d0b20d12db2
LauraRashel <laura.orellana@utp.ac.pa>
2025-07-24 00:27:12 -0500

    Cambioslau

---
+++ src/Pedido.java
@@ -0,0 +1,84 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Pedido extends JFrame {
+    private JButton SARIL$300Button;
+    private JButton GUANDÚ$250Button;
+    private JButton volverButton;
+    private JPanel panelPrincipal;
+
+    public Pedido() {
+        setTitle("Seleccione un Producto");
+        setSize(600, 500);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+
+        panelPrincipal = new JPanel();
+        panelPrincipal.setLayout(null);
+
+        // Título
+        JLabel labelTitulo = new JLabel("Seleccione un Producto");
+        labelTitulo.setFont(new Font("Serif", Font.BOLD, 20));
+        labelTitulo.setBounds(200, 20, 300, 30);
+        panelPrincipal.add(labelTitulo);
+
+        // Botón volver
+        volverButton = new JButton("Volver");
+        volverButton.setBounds(50, 20, 80, 30);
+        panelPrincipal.add(volverButton);
+
+        // Logo
+        JLabel labelLogo = new JLabel();
+        ImageIcon iconLogo = new ImageIcon(getClass().getResource("/grafico/Picture/logo.png"));
+        labelLogo.setIcon(new ImageIcon(iconLogo.getImage().getScaledInstance(120, 120, Image.SCALE_SMOOTH)));
+        labelLogo.setBounds(230, 60, 150, 150);
+        panelPrincipal.add(labelLogo);
+
+        // Imagen y etiqueta SARIL
+        JLabel imgSaril = new JLabel();
+        ImageIcon iconSaril = new ImageIcon(getClass().getResource("/grafico/Picture/saril.png"));
+        imgSaril.setIcon(new ImageIcon(iconSaril.getImage().getScaledInstance(80, 80, Image.SCALE_SMOOTH)));
+        imgSaril.setBounds(100, 250, 80, 80);
+        panelPrincipal.add(imgSaril);
+
+        JLabel labelSaril = new JLabel("SARIL $ 3.00 lb");
+        labelSaril.setBounds(100, 330, 120, 20);
+        panelPrincipal.add(labelSaril);
+
+        SARIL$300Button = new JButton("Agregar SARIL");
+        SARIL$300Button.setBounds(90, 360, 130, 25);
+        panelPrincipal.add(SARIL$300Button);
+
+        // Imagen y etiqueta GUANDÚ
+        JLabel imgGuandu = new JLabel();
+        ImageIcon iconGuandu = new ImageIcon(getClass().getResource("/grafico/Picture/guandu.png"));
+        imgGuandu.setIcon(new ImageIcon(iconGuandu.getImage().getScaledInstance(80, 80, Image.SCALE_SMOOTH)));
+        imgGuandu.setBounds(400, 250, 80, 80);
+        panelPrincipal.add(imgGuandu);
+
+        JLabel labelGuandu = new JLabel("GUANDÚ $ 2.50 lb");
+        labelGuandu.setBounds(400, 330, 120, 20);
+        panelPrincipal.add(labelGuandu);
+
+        GUANDÚ$250Button = new JButton("Agregar GUANDÚ");
+        GUANDÚ$250Button.setBounds(390, 360, 140, 25);
+        panelPrincipal.add(GUANDÚ$250Button);
+
+        // Acción del botón volver
+        volverButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose(); // o cambiar a la pantalla anterior si existe
+            }
+        });
+
+        add(panelPrincipal);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Pedido();
+    }
+}
